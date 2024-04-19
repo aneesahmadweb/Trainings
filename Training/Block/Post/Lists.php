d443d01e2ed96e8a46a83bac1711451417deb1c0
root <root@DESKTOP-FHB3ICV>
2024-04-19 21:45:16 +0500

    update

--- Training/Block/Post/Lists.php
+++ Training/Block/Post/Lists.php
@@ -10,7 +10,7 @@ class Lists extends Template
 {
 
     private $collections;
-
+  static $pagerCounter;
 
     /**
      * Constructor
@@ -29,16 +29,48 @@ class Lists extends Template
         $this->collections = $collections;
     }
 
+
     /**
-     * Example method in custom block
-     *
-     * @return string
+     * @return $this
      */
-    public function getCustomMessage()
+    protected function _prepareLayout()
     {
-        return __('This is a custom message from your block.');
+        parent::_prepareLayout();
+        $this->pageConfig->getTitle()->set(__('My Blog List'));
+        if($this->getcoll()){
+
+
+            // Create a unique pager block ID using a counter
+            $pagerId = 'reward_history_pager_' . self::$pagerCounter++;
+
+            // Create pager block using $records
+            $pager = $this->getLayout()->createBlock(
+                'Magento\Theme\Block\Html\Pager',
+                $pagerId
+            )->setAvailableLimit(array(3=>3,10=>10,15=>15,20=>20))
+                ->setShowPerPage(true)->setCollection(
+                    $this->getcoll()
+                );
+            $this->setChild($pagerId, $pager);
+            $this->getcoll()->load();
+        }
+        return $this;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getPagerHtml()
+    {
+        // Retrieve the unique pager block ID dynamically
+        $pagerId = 'reward_history_pager_' . (self::$pagerCounter - 1);
+
+        // Return the HTML of the pager block with the unique ID
+        return $this->getChildHtml($pagerId);
+    }
+    /**
+     * @return Collection
+     */
     public function getcoll(){
 
        return $this->collections;
